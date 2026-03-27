e189624e10a11f759a1412d5b41b3198b292a9ef
dotToras <odamasenganumeko@gmail.com>
2026-03-26 22:04:59 -0300

    feat: eleicao 2023

---
+++ Eleicao2026/app/src/main/java/com/example/eleicao2026/LoginActivity.java
@@ -0,0 +1,69 @@
+package com.example.eleicao2026;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    String usuario = "Mario";
+    String senha = "23#32";
+    EditText edUsuario, edSenha;
+    Button btEntrar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        btEntrar = findViewById(R.id.btEntrar);
+        edUsuario = findViewById(R.id.edUsuario);
+        edSenha = findViewById(R.id.edSenha);
+
+        btEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = "Mario";
+                String senha = "1234";
+
+                if(edUsuario.getText().toString().equals(email) && edSenha.getText().toString().equals(senha)){
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+
+                    //Mensagem de aviso
+                    String text = "Bem-vindo!";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(LoginActivity.this, text, duration);
+                    toast.show();
+                }
+                else{
+                    //Mensagem de aviso
+                    String text = "Email ou senha incorretos!";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(LoginActivity.this, text, duration);
+                    toast.show();
+                }
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
